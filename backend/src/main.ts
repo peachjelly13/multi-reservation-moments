ade7a4b50dece169a11e8c123ecf46699bf1cfb6
peachjelly13 <ananyasharma112233441234@gmail.com>
2025-05-15 16:24:53 +0530

    db connection file loaded in the entry point/main.ts of the project

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,29 @@
-import dotenv from 'dotenv'; 
-import path from 'path'
-dotenv.config({path:'../../.env'});
+//This is the main entry point of the application
+import {connectDB} from './db/db.ts';
+import {env} from './config/env.ts'
 
 import express from 'express';
+import {logger} from './config/logger.ts';
 const app = express();
 
-const PORT = process.env.PORT
+const PORT = env.PORT
 
-app.listen(PORT,()=>{
-    console.log('app is listening on the port ..');
-})
+connectDB()
+  .then(() => {
+    app.on("error", (err) => {
+      logger.error("Application runtime error", err);
+      throw err;
+    });
+
+    app.listen(PORT, () => {
+      logger.info(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    logger.error("Database connection failed", err);
+    process.exit(1);
+  });
+
+
+
+
