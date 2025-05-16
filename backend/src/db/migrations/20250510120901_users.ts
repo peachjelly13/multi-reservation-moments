48efb7f319c05d7fe2c886850d77588f92dd0be4
peachjelly13 <ananyasharma112233441234@gmail.com>
2025-05-17 04:27:10 +0530

    testing pull request for github app

--- backend/src/db/migrations/20250510120901_users.ts
+++ backend/src/db/migrations/20250510120901_users.ts
@@ -1,8 +1,14 @@
+import { generateKeySync } from "crypto";
 import type { Knex } from "knex";
 
 
 export async function up(knex: Knex): Promise<void> {
-}
+    return knex.schema.createTable("users",function(table){
+        
+        
+
+    });
+};
 
 
 export async function down(knex: Knex): Promise<void> {
